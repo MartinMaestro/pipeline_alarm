2d23ae8081f87fd27a06e5af6e1021c973e7ec9d
Gonzalo Gutierrez Martin <gonzalo.gutierrez.martin.contractor@bbva.com>
2017-06-18 01:09:45 +0200

    Add mqtt call

--- pipeline_checker.sh
+++ pipeline_checker.sh
@@ -2,7 +2,12 @@
 
 while true
 do
+  FAILED="failed"
   OUTPUT="$(curl http://127.0.0.1:8154/go/pipelines.json)"
-  
+  if echo "$OUTPUT" | grep -q "$FAILED"; then
+    mosquitto_pub -h {YOUR_MQTT_HOST} -p 1883 -t {YOUR_TOPIC_NAME} -m "1";
+  else
+    mosquitto_pub -h {YOUR_MQTT_HOST} -p 1883 -t {YOUR_TOPIC_NAME} -m "0";
+  fi
   sleep 2
 done
